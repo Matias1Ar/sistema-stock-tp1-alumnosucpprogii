5ab2ffcdf9276b4d8ae84f3e3c52d16717de7965
Matias1Ar <63749859+Matias1Ar@users.noreply.github.com>
2020-04-15 17:37:51 -0300

    matiasArmoa

--- principal.py
+++ principal.py
@@ -1,4 +1,6 @@
 from tkinter import *
 window = Tk()
 window.title("Programación II - Sistema Stock Delgado")
+lbl_1 = Label(window, text= "Hola", font=("Verdana", 10))
+lbl_1.grid(column=0, row=2)
 window.mainloop()
